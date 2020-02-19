bc884382f4defa9e93c5fa9652b402bdd79fd078
Vladimir <angara99@gmail.com>
2020-02-19 18:27:37 +0300

    Before cleanup

--- programmers/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProgrammersConfig(AppConfig):
-    name = 'programmers'
--- questions/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from questions.models import TestAnswer, Question
-
-admin.site.register(TestAnswer)
-admin.site.register(Question)
