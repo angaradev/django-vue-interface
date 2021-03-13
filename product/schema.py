d6eb8acbafd99741bb15aa1c0fb2dd55d2b7424a
mikohan <angara99@gmail.com>
2021-03-13 20:26:56 +0300

    Django graphene try

---
+++ product/schema.py
@@ -0,0 +1,33 @@
+from product.models.models import CarMake, CarModel
+from graphene import String, ObjectType, Int, ID, Field, Schema, Interface, List
+from graphene_django import DjangoObjectType
+from graphene_django import DjangoListField
+from elasticsearch import Elasticsearch
+import requests, json
+
+
+# connections.create_connection(hosts=["localhost:9200"], timeout=20)
+es = Elasticsearch(["http://localhost:9200"])
+
+
+class CarMakeType(DjangoObjectType):
+    class Meta:
+        model = CarMake
+        fields = ("name", "country")
+
+
+class CarModelType(DjangoObjectType):
+    class Meta:
+        model = CarModel
+        fields = ("name",)
+
+
+class Query(ObjectType):
+
+    vehicle = List(CarModelType)
+
+    def resolve_vehicle(root, info):
+        return CarModel.objects.all()
+
+
+schema = Schema(query=Query)
