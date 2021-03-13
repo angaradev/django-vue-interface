f2f9316341284d9415392d7b5deb94456ae5839f
mikohan <angara99@gmail.com>
2021-03-13 10:51:56 +0300

    Finished with models

--- product/models/models_price.py
+++ product/models/models_price.py
@@ -9,7 +9,10 @@ class Price(models.Model):
     Class handling product prices
     """
 
-    pass
+    product = models.OneToOneField(
+        Product, on_delete=models.CASCADE, related_name="product_price"
+    )
+    value = models.DecimalField(max_digits=20, decimal_places=2, null=True, blank=True)
 
 
 class PriceHistory(models.Model):
@@ -17,7 +20,22 @@ class PriceHistory(models.Model):
     Class handling product price history
     """
 
-    pass
+    product = models.OneToOneField(
+        Product, on_delete=models.CASCADE, related_name="product_price_history"
+    )
+    value = models.DecimalField(max_digits=20, decimal_places=2, null=True, blank=True)
+    dollar_rate = models.FloatField()
+    price_date = models.DateTimeField(auto_now_add=True)
+
+
+class Store(models.Model):
+    """
+    Class for handling stores
+    """
+
+    name = models.CharField(max_length=255)
+    location_city = models.CharField(max_length=50)
+    location_address = models.CharField(max_length=100)
 
 
 class Stock(models.Model):
@@ -25,10 +43,9 @@ class Stock(models.Model):
     Handling products on stock
     """
 
-    pass
-
-
-class Stores(models.Model):
-    """
-    Class for handling stores
-    """
+    product = models.OneToOneField(
+        Product, on_delete=models.CASCADE, related_name="product_stock"
+    )
+    store = models.OneToOneField(
+        Store, on_delete=models.CASCADE, related_name="store_stock"
+    )
