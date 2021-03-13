09f30655dd92b0ae43d7a85491362e27ef79879f
mikohan <angara99@gmail.com>
2021-03-13 14:11:15 +0300

    Json with categories rebuilt and working

--- product/api/views_elastic_v1.py
+++ product/api/views_elastic_v1.py
@@ -1,6 +1,6 @@
 from django.views.generic.base import TemplateView
 from django.http import JsonResponse
-from product.models import Product
+from product.models import Product, Category
 import json, requests
 
 
@@ -10,10 +10,10 @@ def send_json(request):
         query = 1
     data_aggs = json.dumps(
         {
-            "size": 20,
+            "size": 0,
             "query": {"match": {"categories.cat_parent": query}},
             "aggs": {
-                "categories": {"terms": {"field": "categories.cat_name.keyword"}},
+                "categories": {"terms": {"field": "categories.cat_id"}},
                 "brands": {"terms": {"field": "brand.brand_name.keyword"}},
                 "engines": {"terms": {"field": "engines.engine_name.keyword"}},
                 "car_models": {"terms": {"field": "car_model.model_name.keyword"}},
@@ -27,6 +27,24 @@ def send_json(request):
         data=data_aggs,
     )
     response = r.json()
+
+    categories = response["aggregations"]["categories"]["buckets"]
+    rebuilt_cats = []
+    for category in categories:
+        new_cat = Category.objects.get(id=category["key"])
+        rebuilt_cats.append(
+            {
+                "key": category["key"],
+                "doc_count": category["doc_count"],
+                "id": new_cat.id,
+                "name": new_cat.name,
+                "parent": new_cat.parent_id,
+                "layout": new_cat.layout,
+                "type": new_cat.type,
+            }
+        )
+    response["aggregations"]["categories"]["buckets"] = rebuilt_cats
+
     data = response
 
     return JsonResponse(data, safe=False)
