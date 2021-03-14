42b5440cb45f276ed3ce0d0a049b331adfa4d674
mikohan <angara99@gmail.com>
2021-03-14 14:24:57 +0300

    Graphene interface is done

--- product/schema.py
+++ product/schema.py
@@ -8,33 +8,55 @@ import requests, json
 
 
 # connections.create_connection(hosts=["localhost:9200"], timeout=20)
-es = Elasticsearch(["http://localhost:9200"])
+# es = Elasticsearch(["http://localhost:9200"])
 
 
-class EngineType(DjangoObjectType):
-    class Meta:
-        model = Engine
-        fields = ("name",)
-
-
-class CarMakeType(DjangoObjectType):
-    class Meta:
-        model = CarMake
-        fields = ("name", "country")
-
-
-class CarModelType(DjangoObjectType):
-    class Meta:
-        model = CarModel
-        fields = ("name", "engine", "carmake", "slug")
+class NewCarModelType(ObjectType):
+    id = ID()
+    name = String()
+    year = List(String)
+    engine = List(String)
+    slug = String()
+    make = String()
+    country = String()
 
 
 class Query(ObjectType):
 
-    vehicle = List(CarModelType)
+    vehicle = Field(NewCarModelType, id=String())
+    vehicles = List(NewCarModelType)
 
-    def resolve_vehicle(root, info):
-        return CarModel.objects.all()
+    def resolve_vehicle(self, info, id):
+        car = CarModel.objects.get(id=id)
+        years = [car.year_from, car.year_to] if car.year_from else []
+        return {
+            "id": car.id,
+            "name": car.name,
+            "year": years,
+            "engine": car.engine.all(),
+            "slug": car.slug,
+            "make": car.carmake.name,
+            "country": car.carmake.country,
+        }
+
+    def resolve_vehicles(self, info):
+        qs = CarModel.objects.all()
+        lst = []
+        for car in qs:
+            years = [car.year_from, car.year_to] if car.year_from else []
+            lst.append(
+                {
+                    "id": car.id,
+                    "name": car.name,
+                    "year": years,
+                    "engine": car.engine.all(),
+                    "slug": car.slug,
+                    "make": car.carmake.name,
+                    "country": car.carmake.country,
+                }
+            )
+
+        return lst
 
 
 schema = Schema(query=Query)
