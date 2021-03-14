55e01034e677cab5c2a23018a671ca9355a7841f
mikohan <angara99@gmail.com>
2021-03-14 15:24:37 +0300

    Refactored All models

--- product/models/models.py
+++ product/models/models.py
@@ -20,41 +20,7 @@ from mptt.models import MPTTModel, TreeForeignKey
 from product.utils import unique_slug_generator
 from product.models.models_vehicle import CarModel
 from product.models.models_images import ProductImage
-
-
-class Categories(MPTTModel):
-    name = models.CharField(max_length=100)
-    type = models.CharField(max_length=30, default="shop")
-    layout = models.CharField(max_length=30, default="products")
-    parent = TreeForeignKey(
-        "self",
-        null=True,
-        blank=True,
-        related_name="children",
-        db_index=True,
-        on_delete=models.DO_NOTHING,
-    )
-    slug = models.SlugField(blank=True)
-
-    class Meta:
-        verbose_name = "Категории"
-
-    def __str__(self):
-        return self.name + "-" + str(self.level)
-
-    # objects = CategoryManager()
-
-    @property
-    def image(self):
-        return "http://localhost:8000/media/123/555_tf/IMG_4210.jpg"
-
-    # @property
-    # def type(self):
-    #     return "shop"
-
-    @property
-    def items(self):
-        return 123
+from product.models.models_productfields import ProductVideos
 
 
 class Category(MPTTModel):  # MPTT model here for now
@@ -114,59 +80,7 @@ class Category(MPTTModel):  # MPTT model here for now
         return 123
 
 
-class CategoryTags(models.Model):
-    name = models.CharField(max_length=50)
-
-
-class Units(models.Model):  # Units for parts
-    unit_name = models.CharField(max_length=10, default="шт")
-
-    class Meta:
-        verbose_name = "Еденица измерения"
-        verbose_name_plural = "Еденицы измерения"
-
-    def __str__(self):
-        return self.unit_name
-
-
-# Product Videos
-class ProductVideos(models.Model):
-    youtube_id = models.CharField(max_length=45, null=True, blank=True)
-    url = models.URLField(max_length=255, null=True, blank=True)
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
-    product = models.ForeignKey(
-        "Product",
-        on_delete=models.CASCADE,
-        blank=True,
-        null=True,
-        related_name="product_video",
-    )
-
-    class Meta:
-        verbose_name = "Видео"
-        verbose_name_plural = "Видео"
-
-
 # Product Description
-
-
-class ProductDescription(models.Model):
-    title = models.CharField(max_length=255, null=True, blank=True)
-    text = models.TextField(null=True, blank=True)
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
-    product = models.OneToOneField(
-        "Product",
-        on_delete=models.CASCADE,
-        blank=True,
-        null=True,
-        related_name="product_description",
-    )
-
-    class Meta:
-        verbose_name = "Описание товара"
-        verbose_name_plural = "Описания товара"
 
 
 class Store(models.Model):
@@ -344,9 +258,6 @@ class AngaraOld(models.Model):
         return self.name
 
 
-# Crosses parts
-
-
 class Cross(models.Model):
     cross = models.CharField(max_length=50)
     product = models.ForeignKey(
@@ -359,59 +270,6 @@ class Cross(models.Model):
 
     def __str__(self):
         return self.product.name
-
-
-class ProductAttributeName(models.Model):
-    """
-    Класс содержит названия атрибутов.
-    При создании нового атрибута будет проверятся если он есть,
-    то просто добавляем значение.
-    Если нет, то создаем новое название атрибута и сохраняем значение в модели
-    ProductAttribute
-    """
-
-    name = models.CharField(max_length=100)
-
-    class Meta:
-        verbose_name = "Название Атрибута"
-        verbose_name_plural = "Названия Атрибутов"
-
-    def __str__(self):
-        return self.name
-
-
-class ProductAttribute(models.Model):
-    """
-    This model has to manage attributes.
-    It has to be devided by two classes.
-    One class for attribute names.
-    Another one for attributes values.
-    And it has to be connected to Product model.
-    """
-
-    attribute_name = models.ForeignKey(
-        ProductAttributeName, on_delete=models.CASCADE, related_name="atr_name"
-    )
-    attribute_value = models.CharField(
-        max_length=45, null=True, verbose_name="Значение атрибута"
-    )
-    created_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
-    updated_date = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
-    product = models.ForeignKey(
-        "Product",
-        on_delete=models.CASCADE,
-        blank=True,
-        null=True,
-        verbose_name="Относится к Товару",
-        related_name="product_attribute",
-    )
-
-    class Meta:
-        verbose_name = "Атрибут"
-        verbose_name_plural = "Атрибуты"
-
-    def __str__(self):
-        return self.attribute_value
 
 
 ################### Category pre save receiver ####################################
