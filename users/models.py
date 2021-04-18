c9ae73555a8f825103a1fcd8a4180e0407669e78
mikohan <angara99@gmail.com>
2021-04-18 10:21:10 +0300

    Added auto user

--- users/models.py
+++ users/models.py
@@ -7,3 +7,8 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     pass
 
+
+class AutoUser(models.Model):
+    userId = models.CharField(max_length=255, null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
