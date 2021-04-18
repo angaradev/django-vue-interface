b87ea87402baf6716338ee59a4166df591049e34
mikohan <angara99@gmail.com>
2021-04-18 10:25:22 +0300

    Added auto user

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from users.models import CustomUser
+from users.models import CustomUser, AutoUser
 
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ['username', 'email', 'is_staff']
+    list_display = ["username", "email", "is_staff"]
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(AutoUser)
--- users/models.py
+++ users/models.py
@@ -12,3 +12,10 @@ class AutoUser(models.Model):
     userId = models.CharField(max_length=255, null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Автопользователь"
+        verbose_name_plural = "Автопользователи"
+
+    def __str__(self):
+        return self.userId
