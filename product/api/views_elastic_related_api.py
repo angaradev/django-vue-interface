052feb047b6239a8d430980142faabbdc8a654ab
mikohan <angara99@gmail.com>
2021-04-22 11:18:26 +0300

    Making a similar api done

--- product/api/views_elastic_related_api.py
+++ product/api/views_elastic_related_api.py
@@ -6,42 +6,59 @@ import pprint, re
 
 pp = pprint.PrettyPrinter(indent=2)
 
-
+#
+# Searching for similar products by car and name of part
+#
 def similar(request):
     if request.method == "GET":
         q = request.GET.get("q")
-        pp.pprint(q)
+        model = request.GET.get("model")
         """
         Check if search by make slug exists
         """
 
-        # If query has car model and slug
-        query = {
-            "size": 20,
-            "_source": ["id", "name"],
-            "query": {
-                "match": {
-                    "name": {
-                        "query": "string",
-                        "analyzer": "rebuilt_russian",
-                        "fuzziness": "auto",
-                        "operator": "and",
+        if model and q:
+
+            # If query has car model and slug
+            query = {
+                "size": 20,
+                "_source": ["id", "name"],
+                "query": {
+                    "bool": {
+                        "must": [
+                            {"match": {"model.slug.keyword": model}},
+                            {
+                                "match": {
+                                    "name": {
+                                        "query": q,
+                                        "analyzer": "rebuilt_russian",
+                                        "fuzziness": "auto",
+                                        "operator": "and",
+                                    }
+                                }
+                            },
+                        ]
                     }
-                }
-            },
-        }
+                },
+            }
+            data = json.dumps(query)
 
-    r = requests.get(
-        "http://localhost:9200/prod_notebook/_search",
-        headers={"Content-Type": "application/json"},
-        data=query,
-    )
-    if r.status_code != 200:
-        raise ValueError(f"Request cannot be proceeded Status code is: {r.status_code}")
-    response = r.json()
+        r = requests.get(
+            "http://localhost:9200/prod_notebook/_search",
+            headers={"Content-Type": "application/json"},
+            data=data,
+        )
+        if r.status_code != 200:
+            raise ValueError(
+                f"Request cannot be proceeded Status code is: {r.status_code}"
+            )
+        response = r.json()
 
-    # Cheking if aggregation exist in the query
+        # Cheking if aggregation exist in the query
 
-    data = response
+        data = response
 
-    return JsonResponse(data, safe=False)
+        return JsonResponse(data, safe=False)
+
+    else:
+        raise Exception({"Cannot poceed the request, params are suck"})
