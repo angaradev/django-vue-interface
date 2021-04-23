1e4f19528b88b21a2b6c1febca8838215c40f295
mikohan <angara99@gmail.com>
2021-04-23 14:18:13 +0300

    Making Car Home Page

--- company_pages/schema.py
+++ company_pages/schema.py
@@ -1,4 +1,6 @@
 from company_pages.models import CompanyPages
+from blog.models import Post, Categories
+
 from graphene import (
     String,
     ObjectType,
@@ -19,11 +21,31 @@ class PageType(ObjectType):
     title = String()
     textHTML = String()
     text = String()
+    author = String()
+
+
+class CategoryType(ObjectType):
+    slug = String()
+    name = String()
+    image = String()
+
+
+class PostType(ObjectType):
+    id = ID()
+    slug = String()
+    image = String()
+    title = String()
+    textHTML = String()
+    text = String()
+    partsCategory = List(CategoryType)
+    category = List(CategoryType)
 
 
 class Query(ObjectType):
-    page = Field(PageType)
+    page = Field(PageType, slug=String())
     pages = List(PageType)
+    post = Field(PostType, slug=String())
+    posts = List(PostType)
 
     def resolve_page(self, info, slug):
         qs = CompanyPages.objects.get(slug=slug)
@@ -32,6 +54,12 @@ class Query(ObjectType):
     def resolve_pages(self, info):
         qs = CompanyPages.objects.all()
         return qs
+
+    def resolve_post(self, info, slug):
+        return Post.object.get(slug=slug)
+
+    def resolve_posts(self, info):
+        return Post.object.all()
 
 
 schema = Schema(query=Query)
