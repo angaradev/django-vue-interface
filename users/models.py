bd7d91e1296f887c8b02132158a2686d097ec105
mikohan <angara99@gmail.com>
2021-05-11 17:54:36 +0300

    Added image to user

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
+from django.contrib.auth.models import User
 
 
 from django.contrib.auth.models import AbstractUser
@@ -8,9 +9,16 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     email = models.EmailField(_("email address"), unique=True)
-    image = models.ImageField(upload_to=settings.USER_IMAGES, blank=True, null=True)
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=settings.USER_IMAGES, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.email
 
 
 class AutoUser(models.Model):
