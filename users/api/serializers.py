b6140cbd28c96d4e2fc9bcc5d94d19a6872e19f0
mikohan <angara99@gmail.com>
2021-05-13 18:30:09 +0300

    User endpoint update done

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -1,8 +1,54 @@
 from rest_framework import serializers
-from users.models import CustomUser
+from users.models import CustomUser, UserProfile
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        exclude = ("id", "user")
+        extra_kwargs = {
+            "image": {"required": False, "allow_null": True},
+            "phone": {"required": False, "allow_null": True},
+        }
 
 
 class UserDisplaySerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer()
+
     class Meta:
         model = CustomUser
-        fields = "__all__"
+        # fields = ("id", "email", "profile")
+        exclude = (
+            "password",
+            "user_permissions",
+            "groups",
+            "is_superuser",
+        )
+        extra_kwargs = {
+            "username": {"required": False, "allow_null": True},
+            "email": {"required": False, "allow_null": True},
+        }
+
+    def update(self, instance, validated_data):
+        print(instance.profile.phone)
+
+        instance.username = validated_data.get("username", instance.username)
+        instance.first_name = validated_data.get("first_name", instance.first_name)
+        instance.last_name = validated_data.get("last_name", instance.last_name)
+        instance.email = validated_data.get("email", instance.email)
+
+        try:
+            instance.profile.phone = validated_data["profile"]["phone"]
+        except:
+            pass
+
+        try:
+            instance.profile.image = (
+                validated_data["profile"]["image"] or instance.profile.image
+            )
+        except:
+            pass
+        instance.profile.save()
+        instance.save()
+
+        return instance
--- users/api/views.py
+++ users/api/views.py
@@ -4,17 +4,15 @@ from users.api.serializers import UserDisplaySerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.authtoken.models import Token
 from rest_framework import viewsets
+from users.models import CustomUser
 
 
 class CurrentUserAPIVeiw(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
     permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        qs
-        serializer = UserDisplaySerializer(request.user)
-        return Response(serializer.data)
+    serializer_class = UserDisplaySerializer
+    paginator = None
 
 
 class CustomObtainAuthToken(ObtainAuthToken):
@@ -28,6 +26,7 @@ class CustomObtainAuthToken(ObtainAuthToken):
             {
                 "token": token.key,
                 "user": {
+                    "id": token.user.id,
                     "username": token.user.username,
                     "email": token.user.email,
                     "image": token.user.profile.image.url
