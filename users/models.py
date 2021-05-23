35f7f72cf9e690cdea07b370e2ebe7a719d21231
mikohan <angara99@gmail.com>
2021-05-23 13:53:34 +0300

    Changed custom user model

--- users/models.py
+++ users/models.py
@@ -4,16 +4,89 @@ from django.conf import settings
 from django_resized import ResizedImageField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.db import models
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.db.models import Manager
+from django_resized import ResizedImageField
+from django.conf import settings
+
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
 
-from django.contrib.auth.models import AbstractUser
+AUTH_PROVIDERS = {
+    "facebook": "facebook",
+    "google": "google",
+    "twitter": "twitter",
+    "email": "email",
+}
 
 
-class CustomUser(AbstractUser):
-    email = models.EmailField(_("email address"), unique=True)
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+
+        if username is None:
+            raise TypeError("Users sould have a useername")
+        if email is None:
+            raise TypeError("Users sould have a email")
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+        return user
+
+    # objects = Manager["User"]()
+
+    def create_superuser(self, username, email, password=None):
+
+        if password is None:
+            raise TypeError("Password should not be none")
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255, db_index=True)
+    email = models.EmailField(max_length=255, unique=True, db_index=True)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    auth_provider = models.CharField(
+        max_length=255, default=AUTH_PROVIDERS.get("email")
+    )
+    image = ResizedImageField(
+        size=[100, 100],
+        quality=75,
+        crop=["middle", "center"],
+        upload_to=settings.USER_IMAGES,
+        blank=True,
+        null=True,
+    )
+    phone = models.CharField(max_length=50, blank=True, null=True)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ["username"]
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }
 
 
 class UserProfile(models.Model):
