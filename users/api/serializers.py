8d95f8762606b3cf7832554b02f68f199db43eca
mikohan <angara99@gmail.com>
2021-05-25 22:39:46 +0300

    Fixed profile edititng

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -43,16 +43,10 @@ class UserDisplaySerializer(serializers.ModelSerializer):
 
         instance.username = validated_data.get("username", instance.username)
         instance.email = validated_data.get("email", instance.email)
-
-        try:
-            instance.phone = validated_data["phone"]
-        except:
-            pass
-
-        try:
-            instance.image = validated_data["image"] or instance.image
-        except:
-            pass
+        instance.first_name = validated_data.get("first_name", instance.first_name)
+        instance.last_name = validated_data.get("last_name", instance.last_name)
+        instance.phone = validated_data.get("phone", instance.phone)
+        instance.image = validated_data["image"] or instance.image
         instance.save()
 
         return instance
