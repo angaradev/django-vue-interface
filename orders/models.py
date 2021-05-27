267e7a7e426e3b52f892a9cffee87fafb6ad86f7
mikohan <angara99@gmail.com>
2021-05-27 19:15:41 +0300

    Orders model created

--- orders/models.py
+++ orders/models.py
@@ -3,15 +3,15 @@ from django.db import models
 
 class Orders(models.Model):
     class StatusChoices(models.TextChoices):
-        ORDERED = "1", "ПОЛУЧЕН"
-        INPROGRESS = "2", "СОБИРАЕТСЯ"
-        SENT = "3", "ОТПРАВЛЕН"
-        DELIVERED = "4", "ДОСТАВЛЕН"
+        ORDERED = ("ORD", "ПОЛУЧЕН")
+        INPROGRESS = ("PROG", "СОБИРАЕТСЯ")
+        SENT = ("SENT", "ОТПРАВЛЕН")
+        DELIVERED = ("DELIV", "ДОСТАВЛЕН")
 
     date = models.DateTimeField(auto_now_add=True)
     number = models.CharField(max_length=50)
     status = models.CharField(
-        max_length=50, choices=StatusChoices, default=StatusChoices.ORDERED
+        max_length=50, choices=StatusChoices.choices, default=StatusChoices.ORDERED
     )
     total = models.DecimalField(max_digits=14, decimal_places=2)
 
