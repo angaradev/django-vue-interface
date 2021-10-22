b956d8f7f8081c578cb778032364a99c09cf8bc0
mikohan <angara99@gmail.com>
2021-10-22 17:37:40 +0300

    Created yandex market api

--- yandex_market/urls.py
+++ yandex_market/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [path("stock", views.GetStock.as_view(), name="yandex-stock")]
+urlpatterns = [path("stocks", views.GetStock.as_view(), name="yandex-stocks")]
