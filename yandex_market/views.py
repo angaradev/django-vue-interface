af23bee0113cb28b3968a53298b59eaecf5e6b83
mikohan <angara99@gmail.com>
2021-10-22 18:35:50 +0300

    Created yandex autoized token

--- yandex_market/views.py
+++ yandex_market/views.py
@@ -4,6 +4,7 @@ from rest_framework import status, permissions
 from .serializers import GetStockSerializer
 from product.models import Stock
 from datetime import timedelta, datetime
+from django.conf import settings
 
 
 def createResponse(stock, warehouseId):
@@ -26,12 +27,18 @@ class GetStock(APIView):
     permission_classes = [permissions.AllowAny]
 
     def post(self, request):
+        token = request.META.get("HTTP_AUTHORIZATION")
         id_list = request.data.get("skus") or []
         warehouseId = request.data.get("warehouseId")
         qs = Stock.objects.filter(product__id__in=id_list)
         response = {"skus": [createResponse(x, warehouseId) for x in qs]}
-        print(response)
+        print(token)
 
         serializer = GetStockSerializer(response)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if token == settings.YANDEX_MARKET_TOKEN:
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(
+                {"error": "You are must be authorized"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
