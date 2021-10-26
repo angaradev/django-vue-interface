9fdb5ebc6caba04434a74427e8bdbd66656aced5
mikohan <angara99@gmail.com>
2021-10-26 15:39:26 +0300

    Created new updater

--- product/syncronizators/yandex_market_update.py
+++ product/syncronizators/yandex_market_update.py
@@ -212,7 +212,7 @@ data = {
 
 
 def updatePrices(prices):
-    url = "https://api.partner.market.yandex.ru/v2/campaigns/22527279/offer-prices/updates.json"
+    url = f"https://api.partner.market.yandex.ru/v2/campaigns/{settings.CAMPAIGN_ID}/offer-prices/updates.json"
 
     headers = {
         "Authorization": settings.OAUTH_YANDEX_MARKET,
@@ -224,7 +224,7 @@ def updatePrices(prices):
 
 
 def updateProducts(product):
-    url = "https://api.partner.market.yandex.ru/v2/campaigns/22527279/offer-mapping-entries/updates.json"
+    url = "https://api.partner.market.yandex.ru/v2/campaigns/{settings.CAMPAIGN_ID}/offer-mapping-entries/updates.json"
 
     headers = {"Authorization": OAUTH_YANDEX_MARKET, "Content-Type": "application/json"}
 
