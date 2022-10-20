ed6a3f0258d0d162344867b1a7e0167f431ec5dd
mikohan <angara99@gmail.com>
2022-10-20 17:06:54 +0300

    Added some stuff

--- quora/settings.py
+++ quora/settings.py
@@ -6,6 +6,13 @@ from datetime import timedelta
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+if os.environ.get('ELASTIC_HOST'):
+    ELASTIC_URL = os.environ.get('ELASTIC_HOST')
+else:
+    ELASTIC_URL = "http://localhost:9200"
+
+ELASTIC_INDEX = "prod_all"
+
 
 SECRET_KEY = SECRET_KEY
 VUE_DEV = VUE_DEV
